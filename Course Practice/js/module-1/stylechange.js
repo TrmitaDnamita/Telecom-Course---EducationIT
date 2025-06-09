8f038d463dd50f6651e54434c8548397b0c5f44e
TrmitaDnamita <franelasd@gmail.com>
2025-06-08 23:52:30 -0300

    big-modifications

--- Course Practice/js/module-1/stylechange.js
+++ Course Practice/js/module-1/stylechange.js
@@ -1,8 +1,8 @@
 function switchStyle() {
-  const actual-style = document.getElementById('theme-style');
-  if (actual-style.getAttribute('href') === '../../css/module-1/challenge2n_style.css') {
-    actual-style.setAttribute('href', '../../css/module-1/challenge2b_style.css');
+  const actualStyle = document.getElementById('theme-style');
+  if (actualStyle.getAttribute('href') === '../../css/module-1/challenge2n_style.css') {
+    actualStyle.setAttribute('href', '../../css/module-1/challenge2b_style.css');
   } else {
-    actual-style.setAttribute('href', '../../css/module-1/challenge2n_style.css');
+    actualStyle.setAttribute('href', '../../css/module-1/challenge2n_style.css');
   }
 }
